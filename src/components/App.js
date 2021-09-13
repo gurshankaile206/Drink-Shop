d195166de436bb915ef9e266cdec7e95140acb6f
Gurshan Kaile <gurshankaile206@gmail.com>
2021-09-12 20:34:46 -0700

    add all methods to newdrinkformControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Header';
-
+import NewDrinkControl from './NewDrinkControl';
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <NewDrinkControl />
     </React.Fragment>
   );
 }
