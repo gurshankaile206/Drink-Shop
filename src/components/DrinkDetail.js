97f5416c2b24a33d354c123151be0388091e798b
Gurshan Kaile <gurshankaile206@gmail.com>
2021-09-12 21:27:41 -0700

    updat drink details

--- src/components/DrinkDetail.js
+++ src/components/DrinkDetail.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Drink from './Drink.js';
 
 function DrinkDetail(props) {
-  const { drink } = props; 
+  const { drink, onClickingDelete} = props; 
 
   return (
     <React.Fragment>
@@ -12,9 +12,17 @@ function DrinkDetail(props) {
       <h4>Drink Brand: {drink.brand}</h4>
       <h4>Drink Price: {drink.price}</h4>
       <h4>Drink Alcohol Content: {drink.alcoholContent}</h4>
-
+      <Button variant="outline-warning" size="sm" onClick = {props.onClickingEdit}>Update Drink</Button>
+      <Button variant="outline-danger" size="sm" onClick = {() => props.onClickingDelete(drink.id) }>Remove Drink</Button>
+      <hr/>
     </React.Fragment>
   )
+}
+
+DrinkDetail.propTypes = {
+  drink: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 }
 
 export default DrinkDetail;
